35c840be621af2aa443473e3246e5f7d72a30724
unknown <kbhoomika05@gmail.com>
2023-07-19 14:45:28 +0530

    Assignment10 File Handling

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char Do = 'y';
+            while(Do == 'y')
+            {
+                try
+                {
+                    Console.WriteLine("***************************");
+                    Console.WriteLine("1. File Creation");
+                    Console.WriteLine("2. File Reading");
+                    Console.WriteLine("3. File Appending");
+                    Console.WriteLine("4. File Deletion");
+                    Console.WriteLine("Enter your choice: ");
+                    int ch = int.Parse(Console.ReadLine());
+
+                    string path = "D:\\Mphasis\\.net\\Day8\\";
+                    string fname;
+                    Console.WriteLine("Enter file name");
+                    fname = Console.ReadLine();
+                    string fpath = path + fname;
+
+                    switch (ch)
+                    {
+                        case 1:
+                            {
+                                if (File.Exists(fpath))
+                                {
+                                    Console.WriteLine("File already exist!!");
+                                }
+                                else
+                                {
+                                    File.Create(fpath);
+                                    Console.WriteLine("File created!!");
+                                }
+                                Console.WriteLine();
+                                break;
+                            }
+                        case 2:
+                            {
+                                if (File.Exists(fpath))
+                                {
+                                    string[] lines = File.ReadAllLines(fpath);
+                                    foreach (string line in lines)
+                                    {
+                                        Console.WriteLine(line);
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("File not exist");
+                                }
+                                
+                                break;
+                            }
+                        case 3:
+                            {
+                                if (File.Exists(fpath))
+                                {
+                                    StreamWriter sw = File.AppendText(fpath);
+                                    sw.WriteLine("Welcome to stream writing example");
+                                    sw.Dispose();
+                                    sw.Close();
+                                    Console.WriteLine("Created and text is written");
+
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Already Exist");
+                                }
+                                Console.WriteLine();
+                                break;
+                            }
+                        case 4:
+                            {
+                                if (File.Exists(fpath))
+                                {
+                                    File.Delete(fpath);
+                                    Console.WriteLine("File deleted permanently");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("File does't exist");
+                                }
+                                Console.WriteLine();
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Invalid Choice");
+                                break;
+                            }
+                    }
+
+                }
+                catch (Exception ex) { Console.WriteLine("Error!! " + ex.Message); }
+                finally
+                {
+                    Console.WriteLine("Do you want to continue? ");
+                    Do = char.Parse(Console.ReadLine());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
